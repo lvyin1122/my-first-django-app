e0777baaece28c770f562ce9791af615f31f45f4
lvyin1122 <khiwhyzqd@outlook.com>
2020-08-13 11:04:58 +0800

    before implementing geneirc views

--- polls/views.py
+++ polls/views.py
@@ -6,8 +6,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -41,8 +60,8 @@ def detail(request, question_id):
 """
 
 def results(request, question_id):
-	response = "You're looking at the results of question %s."
-	return HttpResponse(response % quesiton_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
